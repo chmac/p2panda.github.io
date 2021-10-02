2bad1c54ed2387406bf4c004a46e569fac0ac07e
sandreae <git@contact.samandreae.com>
2021-10-02 23:27:56 +0100

    Add page for open-rpc docs

---
+++ open-rpc/precache-manifest.ea6bd293bd56151b01612fa82e2796f5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36f4f36e2cc4b79c01c90bcdcb409567",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2f7c8bf6a93f8af2dfb8",
+    "url": "/static/css/2.affbeb7a.chunk.css"
+  },
+  {
+    "revision": "2f7c8bf6a93f8af2dfb8",
+    "url": "/static/js/2.7b644cbf.chunk.js"
+  },
+  {
+    "revision": "97e131545bbe5c3dc4437972b5825b45",
+    "url": "/static/js/2.7b644cbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3adddf012111fc9ba4d4",
+    "url": "/static/js/main.4d702b12.chunk.js"
+  },
+  {
+    "revision": "d204129e8cdaaebd4f2b",
+    "url": "/static/js/runtime-main.c0150491.js"
+  }
+]);
